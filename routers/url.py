e900f9c93f9907a3e96c0727f6a100bdfa441d35
abhishek7kalra <abhishek7.kalra@gmail.com>
2023-06-19 21:28:28 +0530

    deleted tempdb, fixed bug

--- routers/url.py
+++ routers/url.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 import os
 import random
 import string
-from temp_db import url_store
 
 router = APIRouter()
 
@@ -36,8 +35,7 @@ def generate_short_url(length=6):
     characters = string.ascii_letters + string.digits
     while True:
         short_url = ''.join(random.choice(characters) for _ in range(length))
-        if short_url not in url_store:
-            return short_url
+        return short_url
 
 def is_short_url_exists(custom_alias):
     # Check if the custom_alias already exists
